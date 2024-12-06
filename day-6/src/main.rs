2dbb7379f0ef84cab1d54a35b0f27bb6726dd936
Joshua <joshgermon@gmail.com>
2024-12-06 22:55:39 +1100

    Day 6 part 2 MY CPU HURTS

--- day-6/src/main.rs
+++ day-6/src/main.rs
@@ -2,13 +2,15 @@ use std::collections::HashSet;
 
 fn main() {
     let input = include_str!("../input.txt");
-    let (starting_pos, grid) = parse_input(input);
 
-    let result = part_one(starting_pos, grid);
+    let result = part_one(input);
     println!("Total visited: {}", result);
+
+    let result_2 = part_two(input);
+    println!("Total obstacle options: {}", result_2);
 }
 
-#[derive(Debug)]
+#[derive(Debug, Hash, PartialEq, Eq, Clone)]
 enum Direction {
     Left,
     Right,
@@ -53,17 +55,55 @@ fn parse_input(input: &str) -> (Point, Vec<Vec<char>>) {
     (starting_pos.unwrap(), grid)
 }
 
-fn part_one(starting_pos: Point, grid: Vec<Vec<char>>) -> i32 {
-    let mut visited = HashSet::new();
+fn part_one(input: &str) -> i32 {
+    let (starting_pos, grid) = parse_input(input);
+    simulate_guard_path(starting_pos, &grid).unwrap().len() as i32
+}
 
-    let mut point = starting_pos;
-    let mut direction = Direction::Up;
+fn part_two(input: &str) -> i32 {
+    let (starting_pos, mut grid) = parse_input(input);
+    // Do first iteration to get visted points
+    let visited = simulate_guard_path(starting_pos.clone(), &grid).unwrap();
+    let mut obstacle_points = HashSet::new();
+
+    for point in visited {
+        // Add obstacle to grid at point
+        let prev_val = grid[point.y as usize][point.x as usize];
+        grid[point.y as usize][point.x as usize] = '#';
+        let visited = simulate_guard_path(starting_pos.clone(), &grid);
+
+        // Change back
+        grid[point.y as usize][point.x as usize] = prev_val;
+
+        match visited {
+            Ok(_) => continue,
+            Err(_) => { obstacle_points.insert(point.clone()); },
+        };
+
+    }
+
+    obstacle_points.len() as i32
+}
+
+fn simulate_guard_path(starting_pos: Point, grid: &Vec<Vec<char>>) -> Result<HashSet<Point>, String>  {
+    let mut visited = HashSet::new();
+    let mut visited_with_directions = HashSet::new();
+
+    let mut point = starting_pos.clone();
+    let starting_direction = Direction::Up;
+
+    let mut direction = starting_direction.clone();
 
     point.move_in_direction(&direction);
 
     let mut current_char = get_grid_value(&grid, &point);
 
     while current_char.is_some() {
+        if visited_with_directions.contains(&(point.clone(), direction.clone())) {
+            return Err(String::from("LOOP"));
+        }
+
+        visited_with_directions.insert((point.clone(), direction.clone()));
         visited.insert(point.clone());
 
         let mut next_point = point.clone();
@@ -73,27 +113,26 @@ fn part_one(starting_pos: Point, grid: Vec<Vec<char>>) -> i32 {
         match next_char {
             Some(ch) => {
                 if ch == '#' {
-                    println!("Encountered object at next! {:?}", point);
+                    // Rotate direction right & move in that direction
                     direction = rotate_direction_right(&direction);
-                    println!("Rotated direction! {:?}", &direction);
-                    point.move_in_direction(&direction);
-                    current_char = get_grid_value(&grid, &point);
-                } else {
-                    println!("Keeping same direction! {:?}", point);
-                    // continue in current direction
-                    point.move_in_direction(&direction);
-                    current_char = next_char;
+
+                    // This was a problem! Don't turn and move, turn OR move to let the loop
+                    // check if the turn gets stuck too
+                    //
+                    // point.move_in_direction(&direction);
+                    // current_char = get_grid_value(&grid, &point);
+                    continue;
                 }
+                // Continue in current direction
+                point.move_in_direction(&direction);
+                current_char = next_char;
             },
-            None => {
-                // Set next char to current and it'll bail next iter
-                break;
-            }
+            None => break
         }
 
     }
 
-    visited.len() as i32
+    Ok(visited)
 }
 
 fn get_grid_value(grid: &Vec<Vec<char>>, point: &Point) -> Option<char> {
@@ -128,12 +167,21 @@ mod tests {
 #.........
 ......#...";
 
+    const TEST_INPUT_2: &str =".#..
+#..#
+....
+^...
+#...
+.#..";
+
     #[test]
     fn test_part_one() {
-        let (sp, grid) = parse_input(TEST_INPUT);
-        assert_eq!(part_one(sp, grid), 41)
+        assert_eq!(part_one(TEST_INPUT), 41)
     }
 
     #[test]
-    fn test_part_two() {}
+    fn test_part_two() {
+        assert_eq!(part_two(TEST_INPUT), 6);
+        assert_eq!(part_two(TEST_INPUT_2), 1);
+    }
 }
